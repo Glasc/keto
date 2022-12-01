4d2d26ac208084c80f0cb646c1d4982d51df0044
Glasc <63518997+Glasc@users.noreply.github.com>
2022-12-01 15:58:32 -0600

    removed href append on url

--- src/features/products/Categories.tsx
+++ src/features/products/Categories.tsx
@@ -65,7 +65,7 @@ export const Categories = ({ categories }: CategoriesProps) => {
 
   return (
     <section className="pt-10 pb-10">
-      <div className="relative mx-auto max-w-[85rem] px-6 md:flex md:flex-row md:items-start md:gap-8">
+      <div className="relative mx-auto max-w-[90rem] px-6 md:flex md:flex-row md:items-start md:gap-8">
         {/* mobile */}
         <section className="sticky top-2 z-50 flex justify-center pb-6 font-medium md:hidden">
           <ul className="menu menu-horizontal mx-auto">
@@ -91,8 +91,8 @@ export const Categories = ({ categories }: CategoriesProps) => {
                   } text-primary-content`}
                 >
                   <a
-                    href="#categories"
                     onClick={() => {
+                      document?.getElementById("categories")?.scrollIntoView();
                       setToggleNavbarMenu(false);
                       handleCategoryClick(null);
                     }}
@@ -110,8 +110,11 @@ export const Categories = ({ categories }: CategoriesProps) => {
                       key={category._id}
                     >
                       <a
-                        href="#categories"
                         onClick={() => {
+                          document
+                            ?.getElementById("categories")
+                            ?.scrollIntoView();
+
                           setToggleNavbarMenu(false);
                           handleCategoryClick(category._id);
                         }}
@@ -139,8 +142,10 @@ export const Categories = ({ categories }: CategoriesProps) => {
               {/* <a className="no-underline" href="#"> */}
               <a
                 className="mx-auto w-full justify-center rounded-none hover:bg-primary/40"
-                href="#categories"
-                onClick={() => handleCategoryClick(null)}
+                onClick={() => {
+                  handleCategoryClick(null);
+                  document?.getElementById("categories")?.scrollIntoView();
+                }}
               >
                 Todos
               </a>
@@ -158,8 +163,10 @@ export const Categories = ({ categories }: CategoriesProps) => {
                   {/* <a href="#"> */}
                   <a
                     className="mx-auto block w-full justify-center rounded-none border-none py-4 text-center hover:bg-primary/40"
-                    href="#categories"
-                    onClick={() => handleCategoryClick(category._id)}
+                    onClick={() => {
+                      handleCategoryClick(category._id);
+                      document?.getElementById("categories")?.scrollIntoView();
+                    }}
                   >
                     {category.title}
                   </a>
@@ -170,9 +177,9 @@ export const Categories = ({ categories }: CategoriesProps) => {
           </ul>
         </section>
 
-            {/* TODO: cambiar items-start */}
+        {/* TODO: cambiar items-start */}
         <div
-          className={`grid w-full grid-cols-[repeat(auto-fit,minmax(250px,_1fr))] items-start justify-end gap-4 md:gap-6`}
+          className={`grid w-full grid-cols-[repeat(auto-fit,minmax(240px,_1fr))] items-start justify-end gap-4 md:gap-6`}
         >
           {categoriesToShow?.map((category) => {
             const categoryProducts = category.products;
@@ -191,7 +198,7 @@ export const Categories = ({ categories }: CategoriesProps) => {
                     {product?.image && (
                       <img
                         className="w-full max-w-xs select-none"
-                        src={`${product?.image}?w=400&fm=webp`}
+                        src={`${product?.image}?w=500&fm=webp`}
                         alt={product?.description}
                       />
                     )}
@@ -201,14 +208,14 @@ export const Categories = ({ categories }: CategoriesProps) => {
                       {product.title}
                     </h2>
                     <p className="text-sm">{product.description}</p>
-                    <div className="mt-3 empty:mt-0 text-neutral-content/80">
+                    <div className="mt-3 text-neutral-content/80 empty:mt-0">
                       {product?.prices?.length > 0 &&
                         product.prices.map((price, idx) => {
-                          if (!price) return
+                          if (!price) return;
                           return (
                             <p className="text-xs" key={idx}>
                               {price}
-                            </p>  
+                            </p>
                           );
                         })}
                     </div>
